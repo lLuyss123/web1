1b7ad80d1806bc8b198169905710fbdaa1b76897
Luis Mejia <lmejiaidep@gmail.com>
2022-11-01 13:18:42 -0500

    2

---
+++ web1/src/components/Parrafo.jsx
@@ -0,0 +1,9 @@
+import React from  'react' ;
+
+const Parrafo = () => {
+    return (
+        <div><h1>hola mundo react</h1></div>
+    )
+        
+}
+export default Parrafo;
---
+++ web1/src/components/Variable.jsx
@@ -0,0 +1,10 @@
+import React from  'react' ;
+
+const Variable = () => {
+    
+    return (
+        <div><h1>hola mundo react</h1></div>
+    )
+        
+}
+export default Variable;
